38eed46e4389770c2d689e36e62d4d402f76660c
NadiaKaputkina <nadia.kaputkina@gmail.com>
2019-09-23 18:26:17 +0300

    Task "Morse code decoder" completed

--- src/index.js
+++ src/index.js
@@ -38,7 +38,36 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let decodedMorseExpr = '';
+
+    for (let i = 0; i < expr.length; i += 10) {
+        let binaryLetter = expr.substr(i, 10);
+
+        if (binaryLetter === '**********') {
+            decodedMorseExpr += ' ';
+        } else {
+            let morseLetter = '';
+
+            for (let j = 0; j < expr.length; j += 2) {
+                let letter = binaryLetter.substr(j, 2);
+
+                switch (letter) {
+                    case '10':
+                        morseLetter += '.';
+                        break;
+                    case '11':
+                        morseLetter += '-';
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            decodedMorseExpr += MORSE_TABLE[morseLetter];
+        }
+    }
+
+    return decodedMorseExpr;
 }
 
 module.exports = {
